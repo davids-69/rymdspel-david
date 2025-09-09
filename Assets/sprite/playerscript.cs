3cdb9f93815b79b210259f08e9e0107db156f87d
unknown <sspel25dasa@axevalla.fhsk.se>
2025-09-09 16:06:19 +0200

    First push

---
+++ Assets/sprite/playerscript.cs
@@ -0,0 +1,70 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class playerscript : MonoBehaviour
+{
+    public float playerspeed = 11;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //move to postion
+        transform.position = new Vector3(0, -4, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * playerspeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * playerspeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * playerspeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * playerspeed * Time.deltaTime);
+        }
+
+        //WRAPAROUND
+
+        //Om postionen är mindre än -11
+        if (transform.position.y <= -11)
+
+        {
+            //tilldela spelarens en ny postion som är X = 9.67f
+            transform.position = new Vector3(9.67f, transform.position.y, 0);
+                
+            }
+        //Om postionen är mer än 11
+        if (transform.position.x >= 11)
+        {
+            //tilldela spelarens en ny postion som är X = 9.67f
+            transform.position = new Vector3(-9.67f, transform.position.x, 0);
+        }
+        if (transform.position.y <= -4.53)
+
+        {
+            //tilldela spelarens en ny postion som är X = 9.67f
+            transform.position = new Vector3(transform.position.y, 4.53f, 0);
+
+        }
+        //Om postionen är mer än 11
+        if (transform.position.x >= 4.53)
+        {
+            //tilldela spelarens en ny postion som är X = 9.67f
+            transform.position = new Vector3(transform.position.x, -4.53f, 0);
+        }
+    }
+
+}
+
+
+
+
