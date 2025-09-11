cc0cea92af436bcd548957defb875397c8d916c3
Axevalla <ht24arsa@axevalla.fhsk.se>
2025-09-11 11:13:40 +0200

    del 1 klar

--- Assets/sprite/playerscript.cs
+++ Assets/sprite/playerscript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class playerscript : MonoBehaviour
 {
     public float playerspeed = 11;
+    public GameObject projectile;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,35 +32,37 @@ public class playerscript : MonoBehaviour
         {
             transform.Translate(Vector3.down * playerspeed * Time.deltaTime);
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+            
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+        }
+            //WRAPAROUND
 
-        //WRAPAROUND
-
-        //Om postionen är mindre än -11
-        if (transform.position.y <= -11)
+            //Om postionen är mindre än -11
+            if (transform.position.y <= -5)
 
         {
             //tilldela spelarens en ny postion som är X = 9.67f
-            transform.position = new Vector3(9.67f, transform.position.y, 0);
+            transform.position = new Vector3(transform.position.x, 4.9f, 0);
                 
             }
         //Om postionen är mer än 11
         if (transform.position.x >= 11)
         {
             //tilldela spelarens en ny postion som är X = 9.67f
-            transform.position = new Vector3(-9.67f, transform.position.x, 0);
+            transform.position = new Vector3(-10.9f, transform.position.y, 0);
         }
-        if (transform.position.y <= -4.53)
-
+        if (transform.position.y >= 5)
         {
             //tilldela spelarens en ny postion som är X = 9.67f
-            transform.position = new Vector3(transform.position.y, 4.53f, 0);
-
+            transform.position = new Vector3(transform.position.x, -4.9f, 0);
         }
         //Om postionen är mer än 11
-        if (transform.position.x >= 4.53)
+        if (transform.position.x <= -11)
         {
             //tilldela spelarens en ny postion som är X = 9.67f
-            transform.position = new Vector3(transform.position.x, -4.53f, 0);
+            transform.position = new Vector3(10.9f, transform.position.y, 0);
         }
     }
 
