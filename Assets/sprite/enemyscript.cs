0c8b3b409caeb89ad43f51dc4915e256be752802
AlxndrMrmo <all25alma@axevalla.fhsk.se>
2025-09-12 12:05:48 +0200

    rymdspel del 2

--- Assets/sprite/enemyscript.cs
+++ Assets/sprite/enemyscript.cs
@@ -1,16 +1,38 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class enemyscript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector3 moveDirection;
+    private float enemy = 1.0f; // Added this line to define the 'enemy' variable
+    public GameObject enemyspawner; // Reference to the spawn point
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        //move to postion
+        transform.position = new Vector3(0, -4, 0);
+        moveDirection = new Vector3(
+            Random.Range(-1f, 1f),
+            Random.Range(-1f, 1f),
+            0f
+        ).normalized;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(moveDirection * enemy * Time.deltaTime);
+        //move to postion
+        transform.position = new Vector3(0, -4, 0);
+
+        // Start is called before the first frame update
     }
+
+    public Update()
+    }
+    Instantiate(enemySpawner, transform.position, Quaternion.identity);
+    {
 }
